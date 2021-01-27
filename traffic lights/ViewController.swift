431b92e41f7b6947243fd676707d93da68dc7a6e
Maria Mamina <maria.mamina3@gmail.com>
2021-01-27 15:34:20 +0200

    working logics realisation

--- traffic lights/ViewController.swift
+++ traffic lights/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    @IBOutlet var lightChangeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightView.layer.cornerRadius = 65
+        yellowLightView.layer.cornerRadius = 65
+        greenLightView.layer.cornerRadius = 65
+        
+        lightChangeButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
-
-
+    var lightNumber = 0
+    
+    @IBAction func lightChanging() {
+        lightChangeButton.setTitle("Next", for: .normal)
+        lightNumber += 1
+        if lightNumber > 3 {lightNumber = 1}
+        switch lightNumber {
+        case 1:
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        case 2:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        case 3:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+        default:
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        }
+    }
+    
 }
 
